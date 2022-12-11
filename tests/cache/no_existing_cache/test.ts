296205944d3dd542d13b39af440c8a06898f4148
Gareth Sears <gareth@migratehr.com>
2022-12-11 14:33:56 +0000

    Add hashtrees from migrations code

--- tests/cache/no_existing_cache/test.ts
+++ tests/cache/no_existing_cache/test.ts
@@ -8,10 +8,12 @@ import { Config } from '../../../src/util/config'
 import sinon from 'sinon'
 import { cache } from '../../../src/tasks/cache'
 import fsExists from 'fs.promises.exists'
+import { hashElement } from 'folder-hash'
 
 const CACHE_DIR = '.cache'
 const testPath = path.relative(process.cwd(), __dirname)
-const cachePath = path.join(testPath, 'migrations', CACHE_DIR)
+const migrationsPath = path.join(testPath, 'migrations')
+const cachePath = path.join(migrationsPath, CACHE_DIR)
 
 let migrationsDirStub: sinon.SinonStub<[], Promise<string>>
 let cacheNameStub: sinon.SinonStub<[], Promise<string>>
@@ -43,32 +45,44 @@ test('when step size is 1, it should create the correct folder and query', async
   const expected = [
     {
       directoryName: '_2022-12-08T19:45:58.674Z_1',
+      includedMigrations: ['2022-12-08T19_45_58.674Z'],
     },
     {
       directoryName: '2022-12-08T19:45:58.674Z_2022-12-08T19:45:58.685Z_1',
+      includedMigrations: ['2022-12-08T19_45_58.685Z'],
     },
     {
       directoryName: '2022-12-08T19:45:58.685Z_2022-12-08T19:45:58.691Z_1',
+      includedMigrations: ['2022-12-08T19_45_58.691Z'],
     },
     {
       directoryName: '2022-12-08T19:45:58.691Z_2022-12-08T19:45:58.699Z_1',
+      includedMigrations: ['2022-12-08T19_45_58.699Z'],
     },
     {
       directoryName: '2022-12-08T19:45:58.699Z_2022-12-08T19:45:58.706Z_1',
+      includedMigrations: ['2022-12-08T19_45_58.706Z'],
     },
   ]
 
   const responses = await Promise.all(
-    expected.map(async ({ directoryName }) => {
+    expected.map(async ({ directoryName, includedMigrations }) => {
       const dirPath = path.join(cachePath, directoryName)
       t.is(await fsExists(dirPath), true, 'Has directory')
 
       const queryPath = path.join(dirPath, 'query.fql')
       const query = await fs.promises.readFile(queryPath, 'utf8')
 
+      const hashTree = await hashElement(path.join(migrationsPath), {
+        folders: { include: includedMigrations },
+      })
+
+      console.log(hashTree.toString())
+
       return {
         directoryPath: dirPath,
         query,
+        hashTree,
       }
     })
   )
@@ -78,6 +92,9 @@ test('when step size is 1, it should create the correct folder and query', async
 
   const queries = responses.map(({ query }) => query)
   t.snapshot(queries)
+
+  const hashTrees = responses.map(({ hashTree }) => hashTree)
+  t.snapshot(hashTrees)
 })
 
 test('when step size is more than 1, it should create the correct folder and query', async (t: ExecutionContext) => {
